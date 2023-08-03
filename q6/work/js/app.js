932c707700070ba86ab1397fb60bacdf177fa6cc
tsujishouta <koshi51455@gmail.com>
2023-08-04 08:14:11 +0900

    second_push!

--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -6,25 +6,25 @@ $(function(){
     //food-listを取得。
     let foodList = $(".food-list li");
     //変数の値が"all"だった場合
-    if("all" === category){
+    if(category === "all"){
       //リストのすべてを表示する。
       foodList.show();
-      //値をリターンし以降の処理を続ける。
-      return false;
-    }
-    //リスト内ループ処理実装
-    $.each(foodList, function(index, element){
-      //category-typeを取得
+      //全てを選択していないとき
+    } else {
+      //foodListのindexに取得、elementにlistの値を取得
+      $.each(foodList, function(index, element){
+        //categoryTypeに選択されたカテゴリーのタイプを代入
       const categoryType = $(element).data("category-type");
       //もし変数が一致していたら
       if(category === categoryType){
-        //要素を表示する
-        $(element).show();
-        //異なっている場合の処理
-      } else {
-        //要素を非表示にする
-        $(element).hide();
-      };
-    });
+          //要素を表示する
+          $(element).show();
+          //異なっている場合の処理
+        } else {
+          //要素を非表示にする
+          $(element).hide();
+        };
+      });
+    }
   });
 });
